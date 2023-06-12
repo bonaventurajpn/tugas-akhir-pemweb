8be7214d0b9c935751d7d1a26caf66f1720c2f1d
Bona <bnvt15@gmail.com>
2023-06-12 18:06:31 +0700

    Update A

--- resources/lang/id/auth.php
+++ resources/lang/id/auth.php
@@ -12,7 +12,6 @@
 */
 
 return [
-    'failed'   => 'Identitas tersebut tidak cocok dengan data kami.',
-    'password' => 'Kata sandi yang dimasukkan salah.',
+    'failed'   => 'Email atau kata sandi anda salah.',
     'throttle' => 'Terlalu banyak upaya masuk. Silahkan coba lagi dalam :seconds detik.',
 ];
